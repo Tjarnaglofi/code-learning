bdcf2ee21ec30cd5963a40c45a931792a76fc3c0
Tjarnaglofi <62500262+Tjarnaglofi@users.noreply.github.com>
2020-03-23 17:52:30 +0100

    Add files via upload

---
+++ C++/w3resource/EASY/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    /*1. Write a program in C++ to print a welcome text in a separate line.
+    cout << "Hello" << endl << "----------Wait-for-It----------" << endl << "World" << endl;*/
+
+    /*2. Write a program in C++ to print the sum of two numbers.
+    cout << "Sum of X and Y numbers is: " << 13+54;*/
+
+    /*3. Write a program in C++ to find Size of fundamental data types.
+    cout << "The size of char is " << sizeof(char) << endl;
+    cout << "The size of short is " << sizeof(short) << endl;
+    cout << "The size of int is " << sizeof(int) << endl;
+    cout << "The size of long is " << sizeof(long) << endl;
+    cout << "The size of long long is " << sizeof(long long) << endl;
+    cout << "The size of float is " << sizeof(float) << endl;
+    cout << "The size of double is " << sizeof(double) << endl;
+    cout << "The size of long double is " << sizeof(long double) << endl;
+    cout << "The size of bool is " << sizeof(bool) << endl;
+    cout << "The size of string is " << sizeof(string) << endl;*/
+
+    /*4. Write a program in C++ to print the sum of two numbers using variables.
+    int a = 43, b = 88;
+    cout << "Sum of your numbers is: " << a+b << endl;*/
+
+    /*5. Write a program in C++ to check the upper and lower limits of integer.
+    cout << " The maximum limit of int data type :                  " << INT_MAX << endl;
+    cout << " The minimum limit of int data type :                  " << INT_MIN << endl;
+    cout << " The maximum limit of unsigned int data type :         " << UINT_MAX << endl;
+    cout << " The maximum limit of long long data type :            " << LLONG_MAX << endl;
+    cout << " The minimum limit of long long data type :             " << LLONG_MIN << endl;
+    cout << " The maximum limit of unsigned long long data type :   " << ULLONG_MAX << endl;
+    cout << " The Bits contain in char data type :                  " << CHAR_BIT << endl;
+    cout << " The maximum limit of char data type :                 " << CHAR_MAX << endl;
+    cout << " The minimum limit of char data type :                 " << CHAR_MIN << endl;
+    cout << " The maximum limit of signed char data type :          " << SCHAR_MAX << endl;
+    cout << " The minimum limit of signed char data type :          " << SCHAR_MIN << endl;
+    cout << " The maximum limit of unsigned char data type :        " << UCHAR_MAX << endl;
+    cout << " The minimum limit of short data type :                " << SHRT_MIN << endl;
+    cout << " The maximum limit of short data type :                " << SHRT_MAX << endl;
+    cout << " The maximum limit of unsigned short data type :       " << USHRT_MAX << endl;*/
+
+    return 0;
+}
